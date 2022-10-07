37b4f0c61f65695a32b2a6a63f56becd9f9cef63
Antti <antti.kukkonen2@gmail.com>
2022-10-07 19:30:37 +0300

    optimized code and added "game_end" function

--- general/sql.py
+++ general/sql.py
@@ -28,6 +28,13 @@ class sql:
             airport_data = cursor.fetchall()
             # Printing distance
             if airport_data:
-                return airport_data
+                return airport_data[0]
             else:
                 return "ERROR: sql returned empty set"
+
+    def game_end(self, points, screen_name):
+        with self.connection.cursor() as cursor:
+            query = f"INSERT INTO flight_game.europe_game (points, screen_name) " \
+                    f"VALUES (%(points)s, %(screen_name)s);"
+            data = {"points": points, "screen_name": screen_name}
+            cursor.execute(query, data)
