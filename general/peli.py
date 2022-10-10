237796786fac7126c81f044be83984cb66afdae0
Antti <antti.kukkonen2@gmail.com>
2022-10-10 17:02:22 +0300

    yassified topic selection and added feature to show the value of topic when wrong answer is selected

--- general/peli.py
+++ general/peli.py
@@ -8,13 +8,17 @@ class game:
     game_running = False
     connection = connection
     airport_amount = 37
-    topics = {1: "elevation_ft"}
+    topics = {1: "elevation_ft",
+              2: "flights",
+              3: "rating",
+              4: "review_amount"}
 
     def __init__(self):
         self.topic = None
         self.points = 0
         self.screen_name = ""
         self.create_game()
+        self.new = None
 
     def create_game(self):
         while True:  # Game
@@ -32,7 +36,8 @@ class game:
             self.screen_name = input("Input a screen name: ")
 
             while True:  # topic selection
-                self.topic = input(f"Select topic:\n{game.topics}\n")
+                self.print_topics()
+                self.topic = input(f"Select topic: (#)\n")
                 try:
                     self.topic = int(self.topic)
                 except ValueError:
@@ -44,20 +49,20 @@ class game:
                         print("### Invalid topic: out of range")
 
         old = game.connection.new_higher_lower(game.topics[self.topic], random.randrange(0, game.airport_amount))
-        new = game.connection.new_higher_lower(game.topics[self.topic], random.randrange(0, game.airport_amount))
+        self.new = game.connection.new_higher_lower(game.topics[self.topic], random.randrange(0, game.airport_amount))
         while game.game_running:  # The game itself
-            if new and old:  # if the SQL-query return valid data for both airports
-                while new == old:  # Re-roll the new airport if it is the same as the old airport. (no duplicates)
-                    new = game.connection.new_higher_lower(game.topics[self.topic],
-                                                           random.randrange(0, game.airport_amount))
+            if self.new and old:  # if the SQL-query return valid data for both airports
+                while self.new == old:  # Re-roll the new airport if it is the same as the old airport. (no duplicates)
+                    self.new = game.connection.new_higher_lower(game.topics[self.topic],
+                                                                random.randrange(0, game.airport_amount))
                 # INTERFACE
                 print(f"\n\n{game.topics[self.topic]:^128}")
-                print(f"{old[0]:>64}  :  {new[0]:<64}\n"
-                      f"{old[1]:>64}  :  {new[1]:<64}\n"
+                print(f"{old[0]:>64}  :  {self.new[0]:<64}\n"
+                      f"{old[1]:>64}  :  {self.new[1]:<64}\n"
                       f"{old[2]:>64}  :  {'??????':<64}\n")
 
-                self.answer(new[2], old[2])  # Ask the user for input and determine whether it is right or wrong
-                old = new  # (current round new) -> (next round old)
+                self.answer(self.new[2], old[2])  # Ask the user for input and determine whether it is right or wrong
+                old = self.new  # (current round new) -> (next round old)
             else:
                 print("### An error occurred while trying to fetch data from the database")
                 sys.exit()
@@ -74,6 +79,7 @@ class game:
 
     def wrong_answer(self):  # What happens when the user inputs the wrong answer
         print("Wrong answer")
+        print(f"The value was: {self.new[2]}")
         self.end_game()
 
     def answer(self, f_data_new, f_data_old):  # Answering process
@@ -98,3 +104,19 @@ class game:
                 else:
                     self.wrong_answer()
                     break
+
+    def print_topics(self):
+        print("\nChoose from the following topics:")
+        x = 0
+        print(f"{'+---+'}{'-----------------------------------+':>28}\n"
+              f"| # | Topic                             |\n"
+              f"{'+---+'}{'-----------------------------------+':>28}")
+        for item in self.topics.items():
+            if x > 0:
+                print()
+            print("| ", end="")
+            x += 1
+            print(f"{item[0]:>1} | ", end="")
+            print(f"{item[1]:<33}", end="")
+            print(f" | ", end="")
+        print(f"\n{'+---+'}{'-----------------------------------+':>28}")
