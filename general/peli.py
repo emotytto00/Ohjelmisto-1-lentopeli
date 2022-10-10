1f79000ee305015589b6d06b588cb6a28beed9ea
Antti <antti.kukkonen2@gmail.com>
2022-10-10 18:50:28 +0300

    clarified code

--- general/peli.py
+++ general/peli.py
@@ -24,7 +24,7 @@ def print_topics():
 class game:
     game_running = False
     connection = connection
-    airport_amount = 37
+    AIRPORT_AMOUNT = 37
     topics = {1: "elevation_ft",
               2: "flights",
               3: "rating",
@@ -33,13 +33,13 @@ class game:
 
     def __init__(self):
         self.topic = None
+        self.new = None
         self.points = 0
         self.screen_name = ""
         self.create_game()
-        self.new = None
 
     def create_game(self):
-        while True:  # Game
+        while True:  # new game creation
             new_game = input("Start a new game?: (y/n)\n")
             if new_game.lower().startswith("y"):
                 self.start_game()
@@ -47,11 +47,17 @@ class game:
                 print("Quitting...")
                 sys.exit()
 
-    def start_game(self):
+    def start_game(self):  # Game
         if not game.game_running:  # starts a new game if a game isn't already running
             self.points = 0
             game.game_running = True
-            self.screen_name = input("Input a screen name: ")
+
+            while True:  # screen name input
+                self.screen_name = input("Input a screen name: ")
+                if len(self.screen_name) < 25:
+                    break
+                else:
+                    print("### Error: name too long (Max 25 characters)")
 
             while True:  # topic selection
                 print_topics()
@@ -66,14 +72,16 @@ class game:
                     else:
                         print("### Invalid topic: out of range")
 
-        old = game.connection.new_higher_lower(game.topics[self.topic], random.randrange(0, game.airport_amount))
-        self.new = game.connection.new_higher_lower(game.topics[self.topic], random.randrange(0, game.airport_amount))
-        while game.game_running:  # The game itself
+        # Initializing the airports from database
+        old = game.connection.new_higher_lower(game.topics[self.topic], random.randrange(0, game.AIRPORT_AMOUNT))
+        self.new = game.connection.new_higher_lower(game.topics[self.topic], random.randrange(0, game.AIRPORT_AMOUNT))
+
+        while game.game_running:
             if self.new and old:  # if the SQL-query return valid data for both airports
                 while self.new == old:  # Re-roll the new airport if it is the same as the old airport. (no duplicates)
                     self.new = game.connection.new_higher_lower(game.topics[self.topic],
-                                                                random.randrange(0, game.airport_amount))
-                # INTERFACE
+                                                                random.randrange(0, game.AIRPORT_AMOUNT))
+                # Print interface
                 print(f"\n\n{game.topics[self.topic]:^128}")
                 print(f"{old[0]:>64}  :  {self.new[0]:<64}\n"
                       f"{old[1]:>64}  :  {self.new[1]:<64}\n"
@@ -81,15 +89,15 @@ class game:
 
                 self.answer(self.new[2], old[2])  # Ask the user for input and determine whether it is right or wrong
                 old = self.new  # (current round new) -> (next round old)
-            else:
+            else:  # if SQL-query doesn't return valid data for both airports
                 print("### An error occurred while trying to fetch data from the database")
                 sys.exit()
 
     def end_game(self):  # What happens when the game ends
         game.connection.game_end(self.points, self.screen_name, self.topic)  # writes values to database
-        print(f"Final score: {self.points}\n\n\n")
-        leaderboard.print_leaderboard(self.topic)
-        game.game_running = False
+        print(f"Final score: {self.points}\n\n\n")  # prints score
+        leaderboard.print_leaderboard(self.topic)  # prints leaderboard
+        game.game_running = False  # stops game
 
     def right_answer(self):  # What happens when the user inputs the right answer
         self.points += 1
@@ -102,23 +110,27 @@ class game:
 
     def answer(self, f_data_new, f_data_old):  # Answering process
         higher = False
-        while True:
+        while True:  # keep asking until valid higher/lower answer is inputted
             user_answer = input("    Higher or lower?: ")
             try:
                 user_answer = str(user_answer)
             except ValueError:
                 print("### Value Error")
             else:
-                if f_data_new > f_data_old:
+                if f_data_new > f_data_old:  # logic for comparison
                     higher = True
+                # if answer is higher and it's correct
                 if user_answer.lower().startswith("h") and higher:
                     self.right_answer()
                     break
+                # if answer is lower and it's correct
                 elif user_answer.lower().startswith("l") and not higher:
                     self.right_answer()
                     break
+                # if not valid input
                 elif not user_answer.lower().startswith("h") and not user_answer.lower().startswith("l"):
                     print("### Invalid answer format")
+                # if answer is not correct or invalid
                 else:
                     self.wrong_answer()
                     break
