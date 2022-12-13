78837f65f792bbaf456a73a07946d452c18362f5
Antti <antti.kukkonen2@gmail.com>
2022-12-13 03:51:03 +0200

    added config.py

---
+++ general/config.py
@@ -0,0 +1,11 @@
+"""DATABASE DATA"""
+AIRPORT_AMOUNT = 39
+topics = {1: ("elevation_ft", "Elevation from sea level"),
+          2: ("flights", "Daily flights"),
+          3: ("rating", "Average Google star rating"),
+          4: ("review_amount", "Amount of reviews on Google"),
+          5: ("revenue", "Annual revenue")}
+
+"""Internal shared variables. DO NOT MODIFY"""
+connection = None
+
